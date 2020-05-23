76a3159033d14fc0ddb6dcc121413bc4fc3207b1
hajindvlp <harryhajinchung@gmail.com>
2020-05-24 01:40:20 +0900

    Initial Commit

---
+++ PracticalCpp/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+
+int main() {
+
+	std::cout << "Welcome To Practical Cpp" << std::endl;
+	std::cout << "Author : Hajin Chung" << std::endl;
+	std::cout << "This course contains lots of Cpp syntax examples" << std::endl;
+	std::cout << "Also it has advanced and in-depth look of how the code realy works" << std::endl;
+	std::cout << "--- this is still incomplete ---" << std::endl;
+
+	return 0;
+}
